77c14127b77969dd54ac517a64ef859c7645be33
Ainoras Zukauskas <ainoraz@gmail.com>
2020-10-22 15:31:05 +0200

    Added missing service injection

--- billchop-be/billchop-be/Startup.cs
+++ billchop-be/billchop-be/Startup.cs
@@ -35,6 +35,7 @@ namespace BillChopBE
             services.AddBillChopRepositories();
 
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IBillService, BillService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
