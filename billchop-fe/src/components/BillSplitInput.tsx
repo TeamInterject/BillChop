4e8137c162ed861aa0bbfa171e86ce90990929bb
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2020-10-26 19:37:37 +0100

    Last lint errors fix

--- billchop-fe/src/components/BillSplitInput.tsx
+++ billchop-fe/src/components/BillSplitInput.tsx
@@ -3,33 +3,33 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 interface IState {
-  inputValue: string
+  inputValue: string;
 }
 
 interface IProps {
-  onSplit: (amount: number) => void
+  onSplit: (amount: number) => void;
 }
 
-export class BillSplitInput extends React.Component<
-  IProps,
-  IState
-> {
+export default class BillSplitInput extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.handleOnClick = this.handleOnClick.bind(this);
     this.state = {
-      inputValue: ""
-    }
+      inputValue: "",
+    };
   }
 
-  handleOnClick() {
-    this.props.onSplit(+this.state.inputValue)
+  handleOnClick(): void {
+    const { onSplit } = this.props;
+    const { inputValue } = this.state;
+    onSplit(+inputValue);
     this.setState({
-      inputValue: ""
+      inputValue: "",
     });
   }
 
-  render() {
+  render(): JSX.Element {
+    const { inputValue } = this.state;
     return (
       <div>
         <div className="m-2">
@@ -37,14 +37,11 @@ export class BillSplitInput extends React.Component<
           <Form.Control
             placeholder="Enter the amount"
             onChange={(e) => this.setState({ inputValue: e.target.value })}
-            value={this.state.inputValue}
+            value={inputValue}
           />
         </div>
         <div className="m-2">
-          <Button
-            variant="outline-primary"
-            onClick={this.handleOnClick}
-          >
+          <Button variant="outline-primary" onClick={this.handleOnClick}>
             Split
           </Button>
         </div>
