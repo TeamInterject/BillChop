f846c602ac972b8b575ab5ba266058c42d4101eb
Ainoras Zukauskas <ainoraz@gmail.com>
2020-11-05 01:47:10 +0100

    Updated tests

--- billchop-fe/src/tests/components/Sidebar.test.tsx
+++ billchop-fe/src/tests/components/Sidebar.test.tsx
@@ -1,26 +1,52 @@
-import { assert } from "chai";
-import { render } from "@testing-library/react";
+import { expect } from "chai";
+import { render, fireEvent } from "@testing-library/react";
 import React from "react";
 import sinon from "sinon";
 import Sidebar from "../../components/Sidebar";
 
 describe("Sidebar Tests", () => {
-  it("Should render correctly", async () => {
+  it("Should render single group correctly", async () => {
     // Arrange
-    const spy = sinon.spy();
-
     // Act
     const sidebar = render(
       <Sidebar
         sidebarTabs={[{ groupId: "1", groupName: "Test group" }]}
-        onTabClick={spy}
+        onTabClick={() => {}}
       />
     );
 
     // Assert
     const result = await sidebar.findAllByText("Test group");
 
-    assert.equal(result.length, 1);
-    assert.equal(result[0].textContent, "Test group");
+    expect(result.length).to.be.equal(1);
+    expect(result[0].textContent).to.be.equal("Test group");
+  });
+
+  it("Should call onTabClick when sidebar item clicked", async () => {
+    // Arrange
+    const stub = sinon.stub();
+    const expectedGroup = { groupId: "1", groupName: "Test group" };
+
+    // Act
+    const sidebar = render(
+      <Sidebar
+        sidebarTabs={[
+          expectedGroup,
+          { groupId: "2", groupName: "Group for Testing" },
+        ]}
+        onTabClick={stub}
+      />
+    );
+
+    // Assert
+    const result = await sidebar.findAllByText("Test group");
+    expect(result.length).to.be.equal(1);
+
+    expect(stub.callCount).to.be.equal(0);
+
+    fireEvent.click(result[0]);
+
+    expect(stub.callCount).to.be.equal(1);
+    expect(stub.calledWithExactly(expectedGroup));
   });
 });
