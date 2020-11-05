7eac9f31b2fbed362eb3a5428e77cd7d528db269
Martynas Baskys <mbaskys@unibz.it>
2020-11-05 13:08:00 +0100

    Added Email to User

--- billchop-be/billchop-be/Migrations/20201105120247_AddedEmailToUser.Designer.cs
+++ billchop-be/billchop-be/Migrations/20201105120247_AddedEmailToUser.Designer.cs
@@ -4,14 +4,16 @@ using BillChopBE.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BillChopBE.Migrations
 {
     [DbContext(typeof(BillChopContext))]
-    partial class BillChopContextModelSnapshot : ModelSnapshot
+    [Migration("20201105120247_AddedEmailToUser")]
+    partial class AddedEmailToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
