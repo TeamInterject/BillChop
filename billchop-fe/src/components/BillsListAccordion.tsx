eb75655324cad03a51ce278785b52f0fd185f5cb
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2020-11-14 13:03:16 +0100

    Change header color on hover, align items in header

--- billchop-fe/src/components/BillsListAccordion.tsx
+++ billchop-fe/src/components/BillsListAccordion.tsx
@@ -5,6 +5,7 @@ import Group from "../backend/models/Group";
 import BillIcon from "../assets/bill-icon.svg";
 import Dictionary from "../util/Dictionary";
 import GroupTable from "./GroupTable";
+import "../styles/bill-list-accordion.css";
 
 export interface IBillsListAccordionProps {
   group: Group;
@@ -31,9 +32,9 @@ export default class BillsListAccordion extends React.Component<
     return bills.map((bill) => {
       return (
         <Card key={bill.Id}>
-          <Accordion.Toggle className="d-flex" style={{ cursor: "pointer" }} as={Card.Header} eventKey={bill.Id}>
+          <Accordion.Toggle className="bill-list-accordion" as={Card.Header} eventKey={bill.Id}>
             <img className="mr-2" src={BillIcon} height="32px" width="32px" alt="Bill icon" />
-            <div className="ml-2 d-flex justify-content-between flex-grow-1">
+            <div className="ml-2 d-flex justify-content-between align-items-center flex-grow-1">
               <div>{bill.Name}</div>
               <div>{bill.Total.toFixed(2)}</div>
               {/* TODO Add date */}
