76a7a502b5770065629c874ffa31fcda51c8b58d
Ainoras Zukauskas <ainoraz@gmail.com>
2020-12-02 18:13:11 +0100

    Payment model

---
+++ billchop-be/billchop-be/DataAccessLayer/Models/Payment.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using BillChopBE.DataAccessLayer.Models.Interfaces;
+using BillChopBE.DataAccessLayer.Models.Validation;
+using Newtonsoft.Json;
+
+namespace BillChopBE.DataAccessLayer.Models
+{
+    public class Payment : ValidatableModel, IDbModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [JsonIgnore]
+        public Guid PayerId { get; set; }
+
+        [Required]
+        public User Payer { get; set; } = null!;
+
+        [Required]
+        [JsonIgnore]
+        public Guid ReceiverId { get; set; }
+
+        [Required]
+        public User Receiver { get; set; } = null!;
+    }
+}
