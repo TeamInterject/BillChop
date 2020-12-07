ec5e43f90954246949fad17e550d9a5f03311603
Ainoras Žukauskas <AinoraZ@gmail.com>
2020-12-07 13:43:53 +0100

    Hotfix/remove untestable code from cov (#80)
    * Removed unused code. Removed untestable code
    
    * Removed generated or uncoverable frontend files from coverage
    
    * Unused using

--- billchop-be/billchop-be/Validation/ValidatableModel.cs
+++ billchop-be/billchop-be/Validation/ValidatableModel.cs
@@ -1,35 +1,14 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using NetEscapades.Configuration.Validation;
 
 namespace BillChopBE.Validation
 {
     public interface IValidatableModel : IValidatable
     {
-        ValidationContainer SilentValidate();
-    }
-
-    public class ValidationContainer
-    {
-        public bool IsSuccess { get; }
-        public ICollection<ValidationResult> ValidationResults { get; }
-
-        public ValidationContainer(bool isSuccess, ICollection<ValidationResult> validationResults) 
-        {
-            IsSuccess = isSuccess;
-            ValidationResults = validationResults;
-        }
     }
 
     public class ValidatableModel : IValidatableModel
     {
-        public ValidationContainer SilentValidate()
-        {
-            var validationResults = new List<ValidationResult>();
-            var isSuccess = Validator.TryValidateObject(this, new ValidationContext(this), validationResults, true);
-            return new ValidationContainer(isSuccess, validationResults);
-        }
-
         public void Validate() => Validator.ValidateObject(this, new ValidationContext(this), true);
     }
 }
