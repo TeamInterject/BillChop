1e68c4b60b426439b163bf4c59eb994ffdd603ff
Ainoras Zukauskas <ainoraz@gmail.com>
2020-12-07 13:24:15 +0100

    Removed unused code. Removed untestable code

--- billchop-be/billchop-be/Validation/ValidatableModel.cs
+++ billchop-be/billchop-be/Validation/ValidatableModel.cs
@@ -6,30 +6,10 @@ namespace BillChopBE.Validation
 {
     public interface IValidatableModel : IValidatable
     {
-        ValidationContainer SilentValidate();
-    }
-
-    public class ValidationContainer
-    {
-        public bool IsSuccess { get; }
-        public ICollection<ValidationResult> ValidationResults { get; }
-
-        public ValidationContainer(bool isSuccess, ICollection<ValidationResult> validationResults) 
-        {
-            IsSuccess = isSuccess;
-            ValidationResults = validationResults;
-        }
     }
 
     public class ValidatableModel : IValidatableModel
     {
-        public ValidationContainer SilentValidate()
-        {
-            var validationResults = new List<ValidationResult>();
-            var isSuccess = Validator.TryValidateObject(this, new ValidationContext(this), validationResults, true);
-            return new ValidationContainer(isSuccess, validationResults);
-        }
-
         public void Validate() => Validator.ValidateObject(this, new ValidationContext(this), true);
     }
 }
