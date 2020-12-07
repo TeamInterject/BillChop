c22c45510d6f4ee3d2efb132312c2185192b81d2
Ainoras Zukauskas <ainoraz@gmail.com>
2020-12-07 13:34:13 +0100

    Unused using

--- billchop-be/billchop-be/Validation/ValidatableModel.cs
+++ billchop-be/billchop-be/Validation/ValidatableModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using NetEscapades.Configuration.Validation;
 
 namespace BillChopBE.Validation
